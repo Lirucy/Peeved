2cf9cbe13a99276ca3c713f3d7fab8d3ae13d5df
lirucy <rick_hertel77@msn.com>
2021-10-13 21:48:21 -0600

    route edit button to edit post

--- client/src/screens/EditPost.jsx
+++ client/src/screens/EditPost.jsx
@@ -1,4 +1,15 @@
-const EditPost = () => {
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+const EditPost = (props) => {
+    // const params = useParams;
+    // console.log(props.post.params);
+    // const {title, content} = props.post;
+
+    // useEffect(() => {
+
+    // })
+
     return (
         <div>
             <h3>Edit posts here!</h3>
