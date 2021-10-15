e62acc816db29f39f81d28d6b6677955175e4998
lirucy <rick_hertel77@msn.com>
2021-10-15 12:50:58 -0600

    fix

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -5,35 +5,33 @@ import { getCommentById, getPostById, getAllComments, getAllPosts } from "../ser
 const Post = (props) => {
     const { title, content } = props.post;
     const [comment, setComment] = useState('');
-    const [filteredComments, setFilteredComments] = useState([]);
+    // const [filteredComments, setFilteredComments] = useState([]);
     const params = useParams;
-    const postId = params.id;
-    const history = useHistory()
+    // const postId = params.id;
+    // const history = useHistory()
 
 
 
-    useEffect(() => {
-        const fetchedFilteredComments = async () =>{
-            const fetchedComments = await getAllComments();
-            const filtered = fetchedComments.filter((comment)=> comment.postId === props.post?.id)
-            setFilteredComments(filtered)
+    // useEffect(() => {
+    //     const fetchedFilteredComments = async () =>{
+    //         const fetchedComments = await getAllComments();
+    //         const filtered = fetchedComments.filter((comment)=> comment.postId === props.post?.id)
+    //         setFilteredComments(filtered)
            
-        }
-        fetchedFilteredComments()
-    },[props.post])
+    //     }
+    //     fetchedFilteredComments()
+    // },[props.post])
 
   
-    const handleOnSubmit = async (e) => {
-        try {
-            e.preventDefault();
+    // const handleOnSubmit = async (e) => {
+    //     try {
+    //         e.preventDefault();
 
-        } catch (error) {
-            console.error(error.message);
-        }
-    }
+    //     } catch (error) {
+    //         console.error(error.message);
+    //     }
+    // }
     return (
-
-    
         <article id="post-box">
             <h3 id= "post-title">{title}</h3>
             <h4 id="post-content">{content}</h4>
@@ -45,7 +43,6 @@ const Post = (props) => {
             ) : (
                 <></>
             )}
-
         </article>
         
     );
